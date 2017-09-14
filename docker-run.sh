b1697747d8e783e3322e2a3ccb38a881e8eaea17
Rubén Cabrera <rcabrera@praxya.com>
2017-09-14 13:40:37 +0200

    WIP obtener DOCKERHOST

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,3 @@
+# El comando con ifconfig NO VA
 export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 docker-compose -f docker-compose.yml up
