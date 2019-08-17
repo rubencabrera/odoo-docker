3a64dbfacad1e565090b024962d177b27fe5f6dc
Rubén Cabrera <dev@rubencabrera.es>
2019-08-17 11:29:17 +0100

    feat: add contrib compose and push version

--- .travis/docker_push.bash
+++ .travis/docker_push.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_LOGIN" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_LOGIN" --password-stdin
 docker push "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}";
