0d1c8b426d74d515cddc3cac7ae8899a059b9fe2
Rubén Cabrera <dev@rubencabrera.es>
2019-12-16 20:00:31 +0000

    wip: perché la minestra si fredda

--- scripts/render_dockerfile.py
+++ scripts/render_dockerfile.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from jinja2 import FileSystemLoader, Environment
+import get_repos
 
+repo_dict = get_repos
 env = Environment(loader=FileSystemLoader('../'))
 template = env.get_template('Dockerfile.jinja2')
 output_from_parsed_template = template.render(
