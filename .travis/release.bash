706c893e6ff28f941f17053522b84f0a75646f85
Rubén Cabrera <dev@rubencabrera.es>
2020-01-12 12:32:00 +0000

    wip: why would this crap be there but not the one I want?

--- .travis/release.bash
+++ .travis/release.bash
@@ -10,5 +10,13 @@ else
   echo "GH_TOKEN token defined"
 fi
 
+if [ -z "${GITHUB_TOKEN}"]
+then
+  echo "GITHUB_TOKEN not defined";
+  exit 1;
+else
+  echo "GITHUB_TOKEN token defined"
+fi
+
 echo "Building image again"
 docker build -t "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}" .
