eb541cb8e16ec0abe20dfccd94e39cb57bffaaf0
Rubén Cabrera <dev@rubencabrera.es>
2020-01-12 12:37:02 +0000

    wip: same crap

--- .travis/release.bash
+++ .travis/release.bash
@@ -5,18 +5,17 @@ set -e
 if [ -z "${GH_TOKEN}"]
 then
   echo "GH_TOKEN not defined";
-  exit 1;
+  if [ -z "${GITHUB_TOKEN}"]
+  then
+    echo "GITHUB_TOKEN not defined";
+    exit 1;
+  else
+    echo "GITHUB_TOKEN token defined"
+  fi
 else
   echo "GH_TOKEN token defined"
 fi
 
-if [ -z "${GITHUB_TOKEN}"]
-then
-  echo "GITHUB_TOKEN not defined";
-  exit 1;
-else
-  echo "GITHUB_TOKEN token defined"
-fi
 
 echo "Building image again"
 docker build -t "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}" .
