096176e1baff90cadde9d9941010d90d04b65988
Rubén Cabrera <dev@rubencabrera.es>
2020-01-13 09:14:04 +0000

    fix: add docker build in docker release step

--- .travis/docker_push.bash
+++ .travis/docker_push.bash
@@ -8,5 +8,7 @@ then
   exit 1;
 fi
 
+echo "Building image again"
+docker build -t "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}" .
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker push "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}";
